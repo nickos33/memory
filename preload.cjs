97fce64ced25a5ca47c0c7b93b048ef49ef6c5b3
nickos33 <103794257+nickos33@users.noreply.github.com>
2026-07-08 22:50:08 +0800

    Add preload.cjs

---
+++ preload.cjs
@@ -0,0 +1,22 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('memorialAPI', {
+  getAll: () => ipcRenderer.invoke('memorial:getAll'),
+  add: (memorial) => ipcRenderer.invoke('memorial:add', memorial),
+  update: (id, data) => ipcRenderer.invoke('memorial:update', { id, data }),
+  delete: (id) => ipcRenderer.invoke('memorial:delete', id),
+  togglePin: (id) => ipcRenderer.invoke('memorial:togglePin', id),
+  reorder: (ids) => ipcRenderer.invoke('memorial:reorder', ids),
+  exportData: () => ipcRenderer.invoke('memorial:exportData'),
+  importData: () => ipcRenderer.invoke('memorial:importData'),
+});
+
+contextBridge.exposeInMainWorld('widgetAPI', {
+  openMain: () => ipcRenderer.invoke('widget:openMain'),
+  move: (dx, dy) => ipcRenderer.invoke('widget:move', dx, dy),
+  showWidget: () => ipcRenderer.invoke('widget:showWidget'),
+  toggleWidget: () => ipcRenderer.invoke('widget:toggleWidget'),
+  hideWidget: () => ipcRenderer.invoke('widget:hideWidget'),
+  onRefresh: (cb) => ipcRenderer.on('widget:refresh', () => cb()),
+  onRestoreAll: (cb) => ipcRenderer.on('widget:restoreAll', () => cb()),
+});
